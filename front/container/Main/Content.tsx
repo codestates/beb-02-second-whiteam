709830225a5dde62557e5b26d29087aa385ab195
2now <kj109888@gmail.com>
2022-02-25 16:38:44 +0900

    💄 Add Main/Content
    Add content UI, remove scroll bar global

--- front/container/Main/Content.tsx
+++ front/container/Main/Content.tsx
@@ -1,27 +1,34 @@
+import { css } from '@emotion/react';
+import { Typography } from '@mui/material';
+
+const wrapper = css`
+  margin: 12rem 0 28rem 10rem;
+`;
+
+const gradientCss = css`
+  background-image: linear-gradient(92deg, #f35626, #feab3a);
+  background-clip: text;
+  text-fill-color: transparent;
+  animation: hue 10s infinite linear;
+
+  @keyframes hue {
+    from {
+      -webkit-filter: hue-rotate(0deg);
+    }
+    to {
+      -webkit-filter: hue-rotate(-360deg);
+    }
+  }
+`;
+
 function Content() {
   return (
-    <>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-    </>
+    <div css={wrapper}>
+      <Typography variant="h1" component="div" gutterBottom>
+        Trade NFTs, Communicate <br /> and Get{' '}
+        <span css={gradientCss}>Rewords</span>
+      </Typography>
+    </div>
   );
 }
 
